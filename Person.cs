9945ce7474b25ded98970490c4bb560f34bc47d1
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-25 12:38:32 +0530

    Person Class containg fields

---
+++ Person.cs
@@ -0,0 +1,16 @@
+﻿public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Adress { get; set; }
+    public string City { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string Zip { get; set; }
+    public string State { get; set; }
+
+}
+
+
+
+
