68f500306f1c94a8579b6fd34565dc9e0be2dd64
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-25 12:34:12 +0530

    UC1_Adress Book System

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace AdressManagementBook
+{
+    class Program
+    {
+
+
+        public static List<Person> People = new List<Person>();
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome to Adress Book");
+                Console.Clear();
+
+                Person person = new Person();
+
+                Console.Write("Enter First Name: ");
+                person.FirstName = Console.ReadLine();
+
+                Console.Write("Enter Last Name: ");
+                person.LastName = Console.ReadLine();
+
+                Console.Write("Enter Phone Number: ");
+                person.PhoneNumber = Console.ReadLine();
+
+                Console.Write("Enter Address : ");
+                person.Adress = Console.ReadLine();
+
+                Console.WriteLine("Enter City: ");
+                person.City = Console.ReadLine();
+
+                Console.WriteLine("Enter State: ");
+                person.State = Console.ReadLine();
+                        
+                Console.WriteLine("Enter Email: ");
+                person.Email = Console.ReadLine();
+
+                People.Add(person);
+            }
+
+        }
+
+    }
+
